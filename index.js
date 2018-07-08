2f0b9c4c8cde0ade2a59ba168f112f846ed50842
Mark Pena <markdrew53@gmail.com>
2018-07-08 17:15:10 -0500

    dejamos un mensaje en la web

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const { createServer } = require('http')
 const port = process.env.PORT || 3000
 
 function webServer(req, res) {
-    res.end('<h1>Hello Circle Ciii</h1>')
+    res.end(`
+        <h1>Hi, Circle CI with Now :) </h1>
+
+        <h3>Pasos que tendra que hacer Circle CI</h3>
+
+        <ol>
+            <li>Creara un pipline build</li>
+            <li>Creara un pipline para deploy dev</li>
+        </ol>
+
+        si aceptamos el PR hara lo siguiente
+
+        <ol>
+            <li>Creara un pipline build</li>
+            <li>Creara un pipline para deploy production</li>
+        </ol>
+    `)
 }
 
 const server = createServer(webServer)
